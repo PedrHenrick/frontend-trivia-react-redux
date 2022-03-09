174551f07439b8463d3a76f83ad444486cee803d
Pedro Henrick <phmc@ic.ufal.br>
2022-03-09 17:07:14 -0300

    Requisito 01: Criaçao da tela de login, e configuraçao do redux

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
+// lalaland
 import { loginUser } from '../redux/Action';
 
 class Login extends Component {
