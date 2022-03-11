8ba81eb0ff2da2d67f67902f647424aa51d57782
Pedro Henrick <phmc@ic.ufal.br>
2022-03-11 19:58:36 -0300

    Reabrindo pr com alteraçoes e conflitos corrigidos

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
-
+// lalaland
 import { fetchQuestions } from '../services/api';
 import Question from '../components/Question';
 import QuestionTimer from '../components/QuestionTimer';
