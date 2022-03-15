25021c68d285e454a9d71114638b9c260b921420
joekliws <j.wilk@hotmail.com>
2022-03-15 11:25:56 -0300

    requisito 12

--- src/components/Forms/Button.js
+++ src/components/Forms/Button.js
@@ -2,25 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-  const { clicked } = props;
+  const { clicked, dataTestId, btnName } = props;
   return (
     <button
       type="button"
       onClick={ clicked }
-      data-testid="btn-next"
-      id="btn-next"
+      data-testid={ dataTestId }
     >
-      Proximo
+      { btnName }
     </button>
   );
 };
 
 Button.defaultProps = {
   clicked: () => '',
+  dataTestId: '',
 };
 
 Button.propTypes = {
   clicked: PropTypes.func,
+  dataTestId: PropTypes.string,
+  btnName: PropTypes.string.isRequired,
 };
 
 export default Button;
--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import Button from '../components/Forms/Button';
+import Header from '../components/Header';
 
 class Feedback extends Component {
+  handleClickRanking = () => {}
+
+  handleClickPlayAgain = () => {}
+
   render() {
     return (
-      <div data-testid="feedback-text">Feedback</div>
+      <div className="feedback-container">
+        <Header />
+        <div className="feedback_headings">
+          <h2 data-testid="feedback-text">Feedback</h2>
+          <h3>
+            <span>Você acertou 2 Questoes</span>
+            <span>Um total de 20 Pontos</span>
+          </h3>
+        </div>
+        <div className="feedback_controls">
+          <Button clicked={ this.handleClickRanking } btnName="Ranking" />
+          <Button clicked={ this.handleClickPlayAgain } btnName="Play Again" />
+        </div>
+      </div>
     );
   }
 }
